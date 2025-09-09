61c8f24a8c856fb01e62324d26d2f770ca8ae7b6
abdullah0nadeem <abdullah.nadeem@arbisoft.com>
2025-09-09 11:06:49 +0500

    Switch statement.

--- traversy-tutorial/main.js
+++ traversy-tutorial/main.js
@@ -197,3 +197,14 @@ const color = value > 10 ? "red" : "blue";
 console.log(color);
 
 // switch
+switch (color) {
+  case "red":
+    console.log("Color is red");
+    break;
+  case "blue":
+    console.log("Color is blue");
+    break;
+  default:
+    console.log("Color is not red or blue");
+    break;
+}
