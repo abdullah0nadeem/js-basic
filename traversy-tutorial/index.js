a1a8241200060297545c90e0158a567d5d3ae208
abdullah0nadeem <abdullah.nadeem@arbisoft.com>
2025-09-09 13:53:36 +0500

    DOM - Events

--- traversy-tutorial/index.js
+++ traversy-tutorial/index.js
@@ -2,27 +2,55 @@
 
 // Single element selector
 
-const form = document.getElementById("my-form");
-console.log(form);
+// const form = document.getElementById("my-form");
+// console.log(form);
 
-console.log(document.querySelector(".container"));
-console.log(document.querySelector("h1"));
+// console.log(document.querySelector(".container"));
+// console.log(document.querySelector("h1"));
 
 // Multiple element selector
-console.log(document.querySelectorAll(".item")); // used for class, id, tag, return NodeList
-console.log(document.getElementsByClassName("item")); // return HTMLCollection
-console.log(document.getElementsByTagName("li")); // return HTMLCollection
+// console.log(document.querySelectorAll(".item")); // used for class, id, tag, return NodeList
+// console.log(document.getElementsByClassName("item")); // return HTMLCollection
+// console.log(document.getElementsByTagName("li")); // return HTMLCollection
 
-const items = document.querySelectorAll(".item");
-items.forEach((item) => console.log(item));
+// const items = document.querySelectorAll(".item");
+// items.forEach((item) => console.log(item));
 
 // DOM - Manupulation
-const ul = document.querySelector(".items");
+// const ul = document.querySelector(".items");
 // ul.remove();
 // ul.lastElementChild.remove();
-ul.firstElementChild.textContent = "Changed";
-ul.children[1].innerText = "brad";
-ul.lastElementChild.innerHTML = "<h2>Item3<h2>";
+// ul.firstElementChild.textContent = "Changed";
+// ul.children[1].innerText = "brad";
+// ul.lastElementChild.innerHTML = "<h2>Item3<h2>";
 
-const button = document.querySelector(".btn");
-button.style.background = "red";
+// const button = document.querySelector(".btn");
+// button.style.background = "red";
+
+// Events
+// button.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   // console.log(event.target);
+//   // console.log(event.target.className);
+//   document.querySelector("#my-form").style.background = "#ccc";
+//   document.querySelector("body").classList.add("bg-dark");
+//   ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
+// });
+
+// button.addEventListener("mouseover", (event) => {
+//   event.preventDefault();
+//   // console.log(event.target);
+//   // console.log(event.target.className);
+//   document.querySelector("#my-form").style.background = "#ccc";
+//   document.querySelector("body").classList.add("bg-dark");
+//   ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
+// });
+
+// button.addEventListener("mouseout", (event) => {
+//   event.preventDefault();
+//   // console.log(event.target);
+//   // console.log(event.target.className);
+//   document.querySelector("#my-form").style.background = "#ccc";
+//   document.querySelector("body").classList.add("bg-dark");
+//   ul.lastElementChild.innerHTML = "<h1>Hello</h1>";
+// });
