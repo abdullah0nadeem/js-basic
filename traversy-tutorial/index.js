3151e236ee1ab08ea1bfa41d8763fa358f3b4d9b
abdullah0nadeem <abdullah.nadeem@arbisoft.com>
2025-09-09 13:18:33 +0500

    DOM - Manupulation

--- traversy-tutorial/index.js
+++ traversy-tutorial/index.js
@@ -15,3 +15,14 @@ console.log(document.getElementsByTagName("li")); // return HTMLCollection
 
 const items = document.querySelectorAll(".item");
 items.forEach((item) => console.log(item));
+
+// DOM - Manupulation
+const ul = document.querySelector(".items");
+// ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = "Changed";
+ul.children[1].innerText = "brad";
+ul.lastElementChild.innerHTML = "<h2>Item3<h2>";
+
+const button = document.querySelector(".btn");
+button.style.background = "red";
